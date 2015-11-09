2d16f0476f4758ad7d556e499648351a12d144a5
xiaofeng <546989644@qq.com>
2015-11-09 10:07:15 +0800

    搭建框架

---
+++ gitHub/gitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitHub
+//
+//  Created by 罗晓锋 on 15/11/9.
+//  Copyright © 2015年 XFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitHub/gitHub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitHub
+//
+//  Created by 罗晓锋 on 15/11/9.
+//  Copyright © 2015年 XFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
